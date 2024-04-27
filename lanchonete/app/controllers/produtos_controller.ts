edc1bf5418f100956afb8c12dd7f5c1e6a3e00f5
Kaio Victor da Silva <kvictor1711@gmail.com.br>
2024-04-26 21:50:20 -0300

    inicio projeto

---
+++ lanchonete/app/controllers/produtos_controller.ts
@@ -0,0 +1,16 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+
+import Produto from "../models/produto.js"
+
+export default class ProdutosController {
+
+    async index() {
+        return await Produto.all()
+    }
+
+    async store() {
+        return [1, 2, 3]
+    }
+
+
+}
